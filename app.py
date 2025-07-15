ae9fbf81a7fe8ab742e22737817a2e5ce9d97ff9
Sharath SP <139264604+spsharath@users.noreply.github.com>
2025-07-15 15:48:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from supabase import create_client, Client
 import numpy as np
 from datetime import datetime, timedelta
